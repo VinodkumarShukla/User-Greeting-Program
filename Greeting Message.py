67543857e5b90a0bd6b1c7a0e7fd24fea8f5350d
Vinod Kumar <142129156+VinodkumarShukla@users.noreply.github.com>
2023-11-20 18:45:53 -0500

    Add files via upload

---
+++ Greeting Message.py
@@ -0,0 +1,6 @@
+# Get user input for name and age
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Display the greeting message
+print(f"Hello, {name}! You are {age} years old. Welcome!")
